9df72bbfb31e91bc323556a72188b1b7c71e1e16
syedzawadarefin <zawadarefin0@gmail.com>
2024-11-05 13:11:35 +1100

    restarting

--- backend/db.js
+++
@@ -1,8 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./database/catalog.db');
-
-db.serialize(() => {
-    db.run("CREATE TABLE IF NOT EXISTS catalog (id INTEGER PRIMARY KEY, name TEXT, description TEXT, price REAL)");
-});
-
-module.exports = db;
